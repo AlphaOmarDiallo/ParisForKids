29ac264b984b99ea17894b258eabce80ce2ae63c
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-08-27 21:12:05 +0200

    Init

---
+++ app/src/main/java/com/alphaomardiallo/parisforkids/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.alphaomardiallo.parisforkids
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.alphaomardiallo.parisforkids.ui.theme.ParisForKidsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ParisForKidsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ParisForKidsTheme {
+        Greeting("Android")
+    }
+}
