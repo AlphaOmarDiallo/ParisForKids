7212add125286b10f8856ad74655932c09d97769
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-08-30 12:53:57 +0200

    Color modified

--- app/src/main/java/com/alphaomardiallo/parisforkids/ui/theme/Theme.kt
+++ app/src/main/java/com/alphaomardiallo/parisforkids/ui/theme/Theme.kt
@@ -5,26 +5,26 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = primaryColor,
+    primaryVariant = primaryLightColor,
+    secondary = secondaryColor,
+    background = primaryDarkColor,
+    surface = primaryDarkColor,
+    onPrimary = primaryTextColor,
+    onSecondary = secondaryTextColor
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
+    primary = primaryColor,
+    primaryVariant = primaryDarkColor,
+    secondary = secondaryColor,
     background = Color.White,
     surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    onPrimary = primaryTextColor,
+    onSecondary = secondaryTextColor
 )
 
 @Composable
