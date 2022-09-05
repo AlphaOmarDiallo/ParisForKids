6f2793bdce261236fbd053157443259f2e48eb90
Alpha Diallo <alphaomar.diallo@gmail.com>
2022-09-05 09:13:36 +0200

    Constant.kt emptied

--- app/src/main/java/com/alphaomardiallo/parisforkids/domain/Constant.kt
+++ app/src/main/java/com/alphaomardiallo/parisforkids/domain/Constant.kt
@@ -2,7 +2,4 @@ package com.alphaomardiallo.parisforkids.domain
 
 object Constant {
 
-    //QUE FAIRE A PARIS API
-    const val BASE_URL_QFAP_API = "https://opendata.paris.fr/"
-    const val QFAP_URL = "api/records/1.0/search/?"
 }
