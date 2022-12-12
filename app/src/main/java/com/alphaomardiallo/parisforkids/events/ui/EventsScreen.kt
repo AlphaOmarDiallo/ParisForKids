c6a90d06c84bfc99d1bd330c106a73f353b78f22
Alpha <adiallo@exomind.fr>
2022-12-12 23:33:47 +0100

    Created screens

---
+++ app/src/main/java/com/alphaomardiallo/parisforkids/events/ui/EventsScreen.kt
@@ -0,0 +1,40 @@
+package com.alphaomardiallo.parisforkids.events.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun EventsScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Transparent)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Events View",
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 25.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun EventsScreenPreview() {
+    EventsScreen()
+}
