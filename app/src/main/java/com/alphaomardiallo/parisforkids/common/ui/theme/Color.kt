8832282dcf3eff75418f5707579a2328bdb2e237
Alpha <adiallo@exomind.fr>
2022-12-12 17:17:25 +0100

    Ui modified - background image added

--- app/src/main/java/com/alphaomardiallo/parisforkids/common/ui/theme/Color.kt
+++ app/src/main/java/com/alphaomardiallo/parisforkids/common/ui/theme/Color.kt
@@ -2,12 +2,14 @@ package com.alphaomardiallo.parisforkids.common.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val primaryColor = Color(0xFF001e96)
-val primaryLightColor = Color(0xFF5147c8)
-val primaryDarkColor = Color(0xFF000067)
+val primaryColor = Color(0xFF6FABE9)
+val primaryLightColor = Color(0xFF88BFFA)
+val primaryDarkColor = Color(0xFF6F6B8D)
 val secondaryColor = Color(0xFFee2436)
 val secondaryLightColor = Color(0xFFff6360)
 val secondaryDarkColor = Color(0xFFb3000f)
 val primaryTextColor = Color(0xFFffffff)
 val secondaryTextColor = Color(0xFF000000)
 val transparent = Color(0x0000ffff)
+val appBar = Color(0xFF6FABE9)
+val bottomBar = Color(0xFF6F6B8D)
