5d24d8a92f477bed135bf496b1da64b481bb6c87
Alpha <adiallo@exomind.fr>
2022-12-12 23:34:46 +0100

    setting up navigation

--- app/src/main/java/com/alphaomardiallo/parisforkids/main/ui/MainActivity.kt
+++ app/src/main/java/com/alphaomardiallo/parisforkids/main/ui/MainActivity.kt
@@ -7,14 +7,12 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.alphaomardiallo.parisforkids.R
 import com.alphaomardiallo.parisforkids.common.ui.theme.ParisForKidsTheme
@@ -29,10 +27,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val navController = rememberNavController()
-            viewModel.checkIfListEventsWasUpdatedToday()
-            viewModel.checkIfWeatherWasUpdatedToday()
-            viewModel.monitorNetworkStatus()
+
+            getData()
 
             ParisForKidsTheme {
                 // A surface container using the 'background' color from the theme
@@ -41,92 +37,41 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background,
 
                     ) {
-                    Layout(navController = navController, startDestination = "ListActivities")
+                    MainScreen()
                 }
             }
         }
     }
+
+    private fun getData() {
+        viewModel.checkIfListEventsWasUpdatedToday()
+        viewModel.checkIfWeatherWasUpdatedToday()
+        viewModel.monitorNetworkStatus()
+    }
+
 }
 
-private fun getData() {
-}
-
-/**
- * Composables
- */
-
+// Composable
 
 @Composable
-fun Layout(
-    navController: NavHostController,
-    startDestination: String
-) {
+fun MainScreen() {
+
+    val navController = rememberNavController()
+
     Scaffold(
         topBar = {
             TopBar(text = stringResource(id = R.string.app_name_formatted))
         },
         bottomBar = {
-            BottomNav()
+            BottomNavigationBar(navController = navController)
         }
     ) { innerPadding ->
         Box(
             modifier = Modifier.padding(innerPadding)
         ) {
-            MainContent(modifier = Modifier)
+            MainContent()
+            Navigation(navController)
         }
     }
 }
-
-@Composable
-fun TopAppBarActionButton(
-    imageVector: ImageVector,
-    description: String,
-    onClick: () -> Unit
-) {
-    IconButton(onClick = {
-        onClick()
-    }) {
-        Icon(imageVector = imageVector, contentDescription = description)
-    }
-}
-
-@Composable
-fun NavigationHost(
-    modifier: Modifier = Modifier,
-    navController: NavHostController,
-    startDestination: String = ""
-) {
-    NavHost(
-        modifier = modifier,
-        navController = navController,
-        startDestination = startDestination
-    ) {
-
-    }
-}
-
 //Previews
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ParisForKidsTheme {
-        TopBar(text = stringResource(id = R.string.app_name_formatted))
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainContentPreview() {
-    ParisForKidsTheme {
-        MainContent(modifier = Modifier)
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun BottomNavPreview() {
-    ParisForKidsTheme {
-        BottomNavigationBar()
-    }
-}
