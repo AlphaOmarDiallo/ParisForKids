377b0bc166e15fd123a01f3715a4fb17aac90094
alpha <alphaomar.diallo@gmail.com>
2022-12-17 00:08:55 +0200

    UI object created

---
+++ app/src/main/java/com/alphaomardiallo/parisforkids/home/domain/UiEventCard.kt
@@ -0,0 +1,12 @@
+package com.alphaomardiallo.parisforkids.home.domain
+
+data class UiEventCard(
+    val id: String,
+    val priceType: String,
+    val coverImage: String,
+    val zipcode: String,
+    val leadText: String,
+    val title: String,
+    val audience: String,
+    val dateDescription: String
+)
