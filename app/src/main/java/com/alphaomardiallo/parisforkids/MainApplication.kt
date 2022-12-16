1ca8d3e34782910754098530e18e3f0aef093e44
alpha <alphaomar.diallo@gmail.com>
2022-12-16 23:43:29 +0200

    Instantiated analytics

--- app/src/main/java/com/alphaomardiallo/parisforkids/MainApplication.kt
+++ app/src/main/java/com/alphaomardiallo/parisforkids/MainApplication.kt
@@ -1,7 +1,19 @@
 package com.alphaomardiallo.parisforkids
 
 import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application(){
+
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+    override fun onCreate() {
+        super.onCreate()
+
+        firebaseAnalytics = Firebase.analytics
+    }
+}
