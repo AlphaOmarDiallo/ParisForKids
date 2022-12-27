3d93833ded2b9941a5e32f59cc68d8a295c3b5f1
Alpha <adiallo@exomind.fr>
2022-12-27 15:24:04 +0100

    added new spacers

--- app/src/main/java/com/alphaomardiallo/parisforkids/common/ui/component/Spacers.kt
+++ app/src/main/java/com/alphaomardiallo/parisforkids/common/ui/component/Spacers.kt
@@ -11,3 +11,13 @@ import com.alphaomardiallo.parisforkids.R
 fun SmallSpacer(modifier: Modifier = Modifier){
     Spacer(modifier.size(dimensionResource(id = R.dimen.margin_small)))
 }
+
+@Composable
+fun MediumSpacer(modifier: Modifier = Modifier){
+    Spacer(modifier.size(dimensionResource(id = R.dimen.margin_medium)))
+}
+
+@Composable
+fun LargeSpacer(modifier: Modifier = Modifier){
+    Spacer(modifier.size(dimensionResource(id = R.dimen.margin_large)))
+}
