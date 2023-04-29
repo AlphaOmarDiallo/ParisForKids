f5dfd76539e485247e3eb76fa293409d38ca9c2f
Alpha <adiallo@exomind.fr>
2023-04-29 16:09:27 +0200

    refactor

--- app/src/main/java/com/alphaomardiallo/parisforkids/settings/ui/SettingsView.kt
+++ app/src/main/java/com/alphaomardiallo/parisforkids/settings/ui/SettingsView.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
-
 @Composable
 fun SettingsScreen() {
     Column(
